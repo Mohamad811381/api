2089bc2dc29a87e1636006d7e173baf774954e3e
code-learning <130752912+Mohamad811381@users.noreply.github.com>
2024-10-05 19:18:18 +0330

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 [
   {
     "id": 1,
-    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    "price": 109.95,
-    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "title": "کیف دستی",
+    "price": 50,
+    "description": "کیف رو تو دستت بگیر همه جا ببر",
     "category": "men's clothing",
     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
     "rating": {
