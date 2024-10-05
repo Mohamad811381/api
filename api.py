c0accb3535f76103eb4267289019df20f7f7b1bf
code-learning <130752912+Mohamad811381@users.noreply.github.com>
2024-10-05 19:49:49 +0330

    Update api.py

--- api.py
+++ api.py
@@ -17,7 +17,7 @@
     "price": 85,
     "description": "تیشرت بپوش حال کن عزیزم",
     "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    "image": "https://raw.githubusercontent.com/abuanwar072/E-commerce-App-UI-Flutter/refs/heads/master/assets/images/bag_2.png",
     "rating": {
       "rate": 4.1,
       "count": 259
@@ -29,7 +29,7 @@
     "price": 55.99,
     "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
     "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    "image": "https://raw.githubusercontent.com/abuanwar072/E-commerce-App-UI-Flutter/refs/heads/master/assets/images/bag_3.png",
     "rating": {
       "rate": 4.7,
       "count": 500
@@ -41,7 +41,7 @@
     "price": 15.99,
     "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
     "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    "image": "https://raw.githubusercontent.com/abuanwar072/E-commerce-App-UI-Flutter/refs/heads/master/assets/images/bag_4.png",
     "rating": {
       "rate": 2.1,
       "count": 430
@@ -53,7 +53,7 @@
     "price": 695,
     "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
     "category": "jewelery",
-    "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    "image": "https://raw.githubusercontent.com/abuanwar072/E-commerce-App-UI-Flutter/refs/heads/master/assets/images/bag_5.png",
     "rating": {
       "rate": 4.6,
       "count": 400
@@ -65,7 +65,7 @@
     "price": 168,
     "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
     "category": "jewelery",
-    "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    "image": "https://raw.githubusercontent.com/abuanwar072/E-commerce-App-UI-Flutter/refs/heads/master/assets/images/bag_6.png",
     "rating": {
       "rate": 3.9,
       "count": 70
