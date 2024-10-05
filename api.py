82a7fe6c2df1596bed989220335478ea11e99bf4
code-learning <130752912+Mohamad811381@users.noreply.github.com>
2024-10-05 18:53:31 +0330

    Update api.py

--- api.py
+++ api.py
@@ -8,19 +8,19 @@ products = [
         'id': 1,
         'title': 'محصول 1',
         'price': 100000,
-        'image': 'https://example.com/image1.jpg'
+        'image': 'https://dkstatics-public.digikala.com/digikala-products/be908c704a2e73e620bcb59e1c1c51bdf023c7d4_1727594405.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90'
     },
     {
         'id': 2,
         'title': 'محصول 2',
         'price': 200000,
-        'image': 'https://example.com/image2.jpg'
+        'image': 'https://dkstatics-public.digikala.com/digikala-products/be908c704a2e73e620bcb59e1c1c51bdf023c7d4_1727594405.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90'
     },
     {
         'id': 3,
         'title': 'محصول 3',
         'price': 300000,
-        'image': 'https://example.com/image3.jpg'
+        'image': 'https://dkstatics-public.digikala.com/digikala-products/be908c704a2e73e620bcb59e1c1c51bdf023c7d4_1727594405.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/format,webp/quality,q_90'
     },
 ]
 
