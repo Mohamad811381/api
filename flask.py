56f41bd76717256c7b35eaa94425389e1ba1353a
code-learning <130752912+Mohamad811381@users.noreply.github.com>
2024-10-13 16:13:15 +0330

    Update flask.py

--- flask.py
+++ flask.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 # لیست اولیه غذاها
 foods = [
-    {"id": 1, "title": "چلو کباب", "price": 50000, "description": "یک غذای خوشمزه و سنتی", "image": "https://example.com/image1.jpg"},
-    {"id": 2, "title": "کباب", "price": 40000, "description": "کباب مخصوص", "image": "https://example.com/image2.jpg"}
+    {"id": 1, "title": "چلو کباب", "price": 50000, "description": "یک غذای خوشمزه و سنتی", "image": "https://github.com/Expensify/App/blob/main/assets/images/simple-illustrations/simple-illustration__track-shoe.svg"},
+    {"id": 2, "title": "کباب", "price": 40000, "description": "کباب مخصوص", "image": "https://github.com/Expensify/App/blob/main/assets/images/simple-illustrations/simple-illustration__track-shoe.svg"}
 ]
 
 @app.route('/api/foods', methods=['GET'])
