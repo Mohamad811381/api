92b897f95925760a1704273f2d1d6627fc3ce43d
code-learning <130752912+Mohamad811381@users.noreply.github.com>
2024-10-13 16:23:24 +0330

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,28 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# لیست اولیه غذاها
-foods = [
-    {"id": 1, "title": "چلو کباب", "price": 50000, "description": "یک غذای خوشمزه و سنتی", "image": "https://github.com/Expensify/App/blob/main/assets/images/simple-illustrations/simple-illustration__track-shoe.svg"},
-    {"id": 2, "title": "کباب", "price": 40000, "description": "کباب مخصوص", "image": "https://github.com/Expensify/App/blob/main/assets/images/simple-illustrations/simple-illustration__track-shoe.svg"}
+products = [
+    {
+        "id": 1,
+        "name": "Product A",
+        "price": 10.0,
+        "description": "This is a description for Product A.",
+        "image_url": "https://example.com/images/product_a.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Product B",
+        "price": 15.0,
+        "description": "This is a description for Product B.",
+        "image_url": "https://example.com/images/product_b.jpg"
+    },
 ]
 
-@app.route('/api/foods', methods=['GET'])
-def get_foods():
-    return jsonify(foods)
-
-@app.route('/api/foods', methods=['POST'])
-def add_food():
-    new_food = request.json
-    foods.append(new_food)
-    return jsonify(new_food), 201
-
-@app.route('/api/foods/<int:food_id>', methods=['DELETE'])
-def delete_food(food_id):
-    global foods
-    foods = [food for food in foods if food['id'] != food_id]
-    return '', 204
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
 
 if __name__ == '__main__':
     app.run(debug=True)
