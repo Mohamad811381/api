f776e23e7a7a2cde062035654c2144aa9c3e3dfe
code-learning <130752912+Mohamad811381@users.noreply.github.com>
2024-10-14 22:36:39 +0330

    Update api.py

--- api.py
+++ api.py
@@ -17,7 +17,7 @@
     "price": 85,
     "description": "تیشرت بپوش حال کن عزیزم",
     "category": "کیف رو تو دستت بگیر همه جا بب",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/2.png",
     "rating": {
       "rate": 4.1,
       "count": 259
@@ -29,7 +29,7 @@
     "price": 55.99,
     "description": "لورم اپیسم",
     "category": "کیف رو تو دستت بگیر همه جا بب",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/3.png",
     "rating": {
       "rate": 4.7,
       "count": 500
@@ -41,7 +41,7 @@
     "price": 15.99,
     "description": "کیف رو تو دستت بگیر همه جا بب",
     "category": "men's clothing",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/4.png",
     "rating": {
       "rate": 2.1,
       "count": 430
@@ -53,7 +53,7 @@
     "price": 695,
     "description": " سلام ",
     "category": "jewelery",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/5.png",
     "rating": {
       "rate": 4.6,
       "count": 400
@@ -65,7 +65,7 @@
     "price": 168,
     "description": "علیک سلام",
     "category": "jewelery",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/6.png",
     "rating": {
       "rate": 3.9,
       "count": 70
@@ -77,7 +77,7 @@
     "price": 9.99,
     "description": "چلو کباب کوبیده",
     "category": "jewelery",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/7.png",
     "rating": {
       "rate": 3,
       "count": 400
@@ -89,7 +89,7 @@
     "price": 10.99,
     "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
     "category": "jewelery",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/8.png",
     "rating": {
       "rate": 1.9,
       "count": 100
@@ -101,7 +101,7 @@
     "price": 64,
     "description": " زرشک پلو با مرغ",
     "category": "electronics",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/9.png",
     "rating": {
       "rate": 3.3,
       "count": 203
@@ -113,7 +113,7 @@
     "price": 109,
     "description": " زرشک پلو با مرغ)",
     "category": "electronics",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/10.png",
     "rating": {
       "rate": 2.9,
       "count": 470
@@ -125,7 +125,7 @@
     "price": 109,
     "description": " زرشک پلو با مرغ",
     "category": "electronics",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/11.png",
     "rating": {
       "rate": 4.8,
       "count": 319
@@ -137,7 +137,7 @@
     "price": 114,
     "description": " زرشک پلو با مرغ",
     "category": "electronics",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/12.png",
     "rating": {
       "rate": 4.8,
       "count": 400
@@ -149,7 +149,7 @@
     "price": 599,
     "description": " زرشک پلو با مرغ",
     "category": "electronics",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/13.png",
     "rating": {
       "rate": 2.9,
       "count": 250
@@ -161,7 +161,7 @@
     "price": 999.99,
     "description": " زرشک پلو با مرغ",
     "category": "electronics",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/14.png",
     "rating": {
       "rate": 2.2,
       "count": 140
@@ -173,7 +173,7 @@
     "price": 56.99,
     "description": " زرشک پلو با مرغ",
     "category": "women's clothing",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/15.png",
     "rating": {
       "rate": 2.6,
       "count": 235
@@ -185,7 +185,7 @@
     "price": 29.95,
     "description": " زرشک پلو با مرغ",
     "category": "women's clothing",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/16.png",
     "rating": {
       "rate": 2.9,
       "count": 340
@@ -197,7 +197,7 @@
     "price": 39.99,
     "description": " زرشک پلو با مرغ",
     "category": "women's clothing",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/17.png",
     "rating": {
       "rate": 3.8,
       "count": 679
@@ -209,7 +209,7 @@
     "price": 9.85,
     "description": " زرشک پلو با مرغ",
     "category": "women's clothing",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/18.png",
     "rating": {
       "rate": 4.7,
       "count": 130
@@ -221,7 +221,7 @@
     "price": 7.95,
     "description": " زرشک پلو با مرغ",
     "category": "women's clothing",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/19.png",
     "rating": {
       "rate": 4.5,
       "count": 146
@@ -233,7 +233,7 @@
     "price": 12.99,
     "description": " زرشک پلو با مرغ",
     "category": "women's clothing",
-    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/1.png",
+    "image": "https://raw.githubusercontent.com/Mohamad811381/imagesfoodsy/refs/heads/main/20.png",
     "rating": {
       "rate": 3.6,
       "count": 145
